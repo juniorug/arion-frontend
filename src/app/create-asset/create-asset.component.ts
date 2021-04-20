d023e8c31a1996d48bfee62821e57c06c1db2465
juniorug <juniorug@gmail.com>
2021-04-20 00:19:26 -0300

    adding forms to add actor and step

--- src/app/create-asset/create-asset.component.ts
+++ src/app/create-asset/create-asset.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, FormArray, Validators} from '@angular/forms';
 
 @Component({
@@ -10,26 +10,23 @@ export class CreateAssetComponent implements OnInit {
 
   assetFormGroup: FormGroup;
   actorsFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
+  stepsFormGroup: FormGroup;
   isEditable = true;
 
+  actors: FormArray;
+  steps: FormArray;
 
-  arrayActors: {
-    id: number;
-    name: string;
-    type: string;
-  }[];
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(private _formBuilder: FormBuilder, private _ref: ChangeDetectorRef) {
     this.assetFormGroup = this._formBuilder.group({
       assetName: ['', Validators.required],
       description: ['']
     });
     this.actorsFormGroup = this._formBuilder.group({
-      actorsFormArray: this._formBuilder.array([])
+      actors: this._formBuilder.array([ this.createActor()  ])
     });
-    this.thirdFormGroup = this._formBuilder.group({
-      thirdCtrl: ['', Validators.required]
+    this.stepsFormGroup = this._formBuilder.group({
+      steps: this._formBuilder.array([ this.createStep()  ])
     });
   }
 
@@ -37,9 +34,6 @@ export class CreateAssetComponent implements OnInit {
     $(window).ready(()=>{
       document.getElementsByClassName("asset-menu")[0].classList.add("active");
     });
-
-    this.arrayActors = [];
-    this.addItem();
   }
 
   ngOnDestroy(): void {
@@ -48,23 +42,48 @@ export class CreateAssetComponent implements OnInit {
     });
   }
 
-  get actorsFormArray() {
-    return this.actorsFormGroup.get('actorsFormArray') as FormArray;
+  createActor(): FormGroup {
+    return this._formBuilder.group({
+      id: [''],
+      name: [''],
+      type: ['']
+    });
   }
 
-  addItem() {
-    console.log("addItem called");
-    
-    let newActor = {
-      id: this.actorsFormArray.length + 1,
-      name:  "",
-      type: "",
-    }
-    this.arrayActors.push(newActor);
-    this.actorsFormArray.push(this._formBuilder.control(false));
+  addActor(): void {
+    this.actors = this.actorsFormGroup.get('actors') as FormArray;
+    this.actors.push(this.createActor());
+    this._ref.detectChanges();
   }
-  removeItem() {
-    this.arrayActors.pop();
-    this.actorsFormArray.removeAt(this.actorsFormArray.length - 1);
+
+  createStep(): FormGroup {
+    return this._formBuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      type: ['', Validators.required]
+    });
   }
+
+  addStep(): void {
+    this.steps = this.stepsFormGroup.get('steps') as FormArray;
+    this.steps.push(this.createStep());
+    this._ref.detectChanges();
+  }
+
+  resetForm() {
+    this.actors.clear();
+    this.steps.clear();
+    this.resetFormGroup(this.assetFormGroup);
+    this.resetFormGroup(this.actorsFormGroup);
+    this.resetFormGroup(this.stepsFormGroup);
+    this.addActor();
+    this.addStep();
+  }
+
+  resetFormGroup(form: FormGroup ) {
+    form.reset(form.value);
+    form.markAsPristine();
+    form.markAsUntouched();
+  }
+
 }
